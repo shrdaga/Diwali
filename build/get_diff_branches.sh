fbebddf597d5d0711b79259ba9bd9c9c2b0fed15
TestKamal <iamdev.kamal@gmail.com>
2018-09-24 22:22:21 +0530

    fix

---
+++ build/get_diff_branches.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export LESSCHARSET=utf-8
+echo $ghprbTargetBranch
+
+echo 'Starting git diff'
+cd ..
+echo 'Switching to origin/'$ghprbTargetBranch
+git checkout -f origin/$ghprbTargetBranch
+echo 'Merging target branch (origin/'$1') into current branch (origin/'$ghprbTargetBranch')'
+git merge --no-commit --no-ff origin/$1
+echo 'Merged. Looking for conflicts'
+git ls-files -u | awk '{$1=$2=$3=""; print $0}' | awk '{ sub(/^[ \t]+/, ""); print }' | sort -u > conflicts.txt
+if [ -s conflicts.txt ]
+then
+    echo 'Conflicts found. Please resolve conflicts prior validation...'
+    echo '============================================================='
+    echo -e '\n\nConflicting files:\n'
+    cat conflicts.txt
+    exit 1
+else
+    echo 'Starting git diff'
+    git status -s > diff.txt
+    echo 'Git diff done.'
+    echo 'Clean double quotes'
+    sed -i 's/\"//g' diff.txt
+    echo 'Cleaned. Result:'
+    cat diff.txt
+fi
